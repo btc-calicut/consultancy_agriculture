1f98830678223b3a13ac768824e298cefbec0fba
emvk09 <edwinmoncy@gmail.com>
2023-08-27 21:32:51 +0530

    fix: Navigation using id

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Menu, Drawer, Button } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 import logo from "../../assets/logo.png";
@@ -8,12 +8,9 @@ const HeaderComponent = () => {
   const [selectedKey, setSelectedKey] = useState("about");
   const [drawerVisible, setDrawerVisible] = useState(false);
   const navigate = useNavigate();
+  const location = useLocation();
 
   const MenuContents = [
-    {
-      label: "About",
-      key: "about",
-    },
     {
       label: "Enquiry",
       key: "enquiry",
@@ -22,13 +19,28 @@ const HeaderComponent = () => {
       label: "Location",
       key: "location",
     },
+    {
+      label: "About",
+      key: "about",
+    },
   ];
 
   const handleMenuClick = (item) => {
     setSelectedKey(item.key);
-    navigate(`/#${item.key}`);
+    // first navigate to landing page and then scroll to section
+    navigate("/");
+    setTimeout(() => {
+      scrollToComponent(item.key);
+    }, 100);
     if (drawerVisible) {
       setDrawerVisible(false);
+    }
+  };
+
+  const scrollToComponent = (componentId) => {
+    const element = document.getElementById(componentId);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
     }
   };
 
@@ -37,9 +49,9 @@ const HeaderComponent = () => {
       <div className="rounded-full w-20 h-20 overflow-hidden">
         <img alt="" src={logo} className="object-cover h-full w-full" />
       </div>
-      <div className="hidden md:flex">
+      <div className="hidden md:flex w-full">
         <Menu
-          className="border-none"
+          className="border-none w-full flex flex-row-reverse"
           theme="light"
           mode="horizontal"
           selectedKeys={[selectedKey]}
@@ -53,11 +65,17 @@ const HeaderComponent = () => {
           onClick={() => setDrawerVisible(true)}
         />
         <Drawer
+          height={280}
+          style={{ background: "rgba(225, 226, 227, 0.99)" }}
           open={drawerVisible}
           onClose={() => setDrawerVisible(false)}
           placement="top"
         >
           <Menu
+            style={{
+              background: "transparent",
+              border: "none",
+            }}
             theme="light"
             mode="vertical"
             selectedKeys={[selectedKey]}
