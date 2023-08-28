7e278aafa185f720b7f353e5b296c63690c246bf
emvk09 <edwinmoncy@gmail.com>
2023-08-28 11:00:19 +0530

    fix: Add sections folder

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import Header from "../../components/Header/Header";
-import About from "../../components/About/About";
-import Enquiry from "../../sections/Enquiry/Enquiry";
 import Footer from "../../Components/Footer/Footer";
 import CardTransition from "../../sections/CardTransition/CardTransition";
 import LinkToProducts from "../../sections/LinkToProducts/LinkToProducts";
+import About from "../../sections/About/About";
+import Enquiry from "../../sections/Enquiry/Enquiry";
 import Location from "../../sections/Location/Location";
 import ScrollToTop from "../../components/ScrollToTop/ScrollToTop";
 
