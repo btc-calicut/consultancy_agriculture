444c49be1185723d28b252178d4aad24c780956e
emvk09 <edwinmoncy@gmail.com>
2023-09-02 20:10:29 +0530

    Fix: Add custom components

--- sections/LinkToProducts.jsx
+++ sections/LinkToProducts.jsx
@@ -9,7 +9,7 @@ const LinkToProducts = () => {
       <Carousel dots={false} autoplay>
         {data.picture.map((item, index) => {
           return (
-            <div className="h-64 xs:h-96">
+            <div className="h-40 xs:h-96">
               <Image
                 alt={item.name}
                 key={index}
@@ -28,13 +28,14 @@ const LinkToProducts = () => {
         })}
       </Carousel>
 
-      <div className="flex items-center xs:absolute inset-0 ">
-        <div className="py-7 px-4 sm:px-10 md:px-14 lg:px-36">
+      <div className="max-xs:bg-zinc-100 flex items-center xs:absolute xs:inset-0">
+        <div className="py-3 xs:py-7 px-4 sm:px-10 md:px-14 lg:px-36">
           <div className="flex flex-col justify-center max-sm:items-center">
             <div className="flex flex-col gap-x-3 max-lg:gap-y-5">
-              <h1 className="text-[27px] xs:text-[40px] font-[1000] max-sm:text-center sm:leading-20 lg:leading-loose bg-gradient-to-r from-sky-500 to-indigo-500 bg-clip-text text-transparent">
-                Cultivating Quality, Harvesting Opportunities,
-                <br className="hidden xs:block" />
+              <h1 className="text-[27px] xs:text-[40px] font-[1000] max-sm:text-center sm:leading-20 lg:leading-loose bg-gradient-to-r from-sky-700 to-sky-500 bg-clip-text text-transparent">
+                Cultivating Quality, <br className="max-xs:block" />
+                Harvesting Opportunities,
+                <br className="max-xs:block sm:block" />
                 Trading the Future
               </h1>
               <p className="max-sm:hidden text-md w-full md:w-80 max-h-60 text-white md:leading-7">
@@ -45,7 +46,7 @@ const LinkToProducts = () => {
 
             <Link
               href="/products"
-              className="relative w-40 h-10 my-5 px-6 py-3 inline-flex items-center justify-center bg-blue-950 border-blue-950 border-2 rounded-full shadow-lg overflow-hidden transition duration-300 ease-out group"
+              className="relative w-40 h-10 my-2 xs:my-5 px-6 py-3 inline-flex items-center justify-center bg-blue-950 border-blue-950 border-2 rounded-full shadow-lg overflow-hidden transition duration-300 ease-out group"
             >
               <span className="absolute inset-0 w-full h-full flex items-center justify-center text-indigo-900 bg-white duration-300 -translate-x-full group-hover:translate-x-0 ease">
                 <svg
--- sections/Location.jsx
+++ sections/Location.jsx
@@ -1,7 +1,7 @@
 const Location = () => {
   return (
     <div id="location" className="bg-white h-80 w-full">
-      <div className="px-4 sm:px-10 md:px-14 lg:px-36 py-7">
+      <div className="px-4 sm:px-10 md:px-14 lg:px-36 py-9">
         <h1 className="text-[#0b0924] font-semibold text-[27px] xs:text-[35px] leading-normal w-full">
           We are here...
         </h1>
