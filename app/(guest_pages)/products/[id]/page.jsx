049dc2a3c1db0e39216416cb91daba177c06a1d5
Aswin K <aswinkarat@gmail.com>
2023-09-28 22:49:13 +0530

    Feat: Connected client product page to database

--- app/(guest_pages)/products/[id]/page.jsx
+++ app/(guest_pages)/products/[id]/page.jsx
@@ -6,18 +6,30 @@ import { Breadcrumb } from "antd";
 import Link from "next/link";
 
 const ProductsPage = () => {
-  const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState(null);
+
   const pathname = usePathname();
   const productName = pathname.replace("/products/", "").replace("%20", " ");
 
   useEffect(() => {
-    // Replace this fetch URL with your actual data URL
-    fetch("/assets/data.json")
-      .then((response) => response.json())
-      .then((data) =>
-        setProducts(data.products.find((item) => item.name === productName))
-      );
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`/api/getproducts`, {
+        method: "POST",
+        body: JSON.stringify(productName),
+      });
+      const data = await response.json();
+      setProduct(data.info);
+      if (response.status === 401) {
+        message.error("Product not found");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <section className="w-full px-4 py-6">
@@ -34,31 +46,31 @@ const ProductsPage = () => {
           },
         ]}
       />
-      {products && (
+      {product && (
         <div className="px-4">
-          <h2 className="text-2xl font-semibold my-6">{products.name}</h2>
+          <h2 className="text-2xl font-semibold my-6">{product.name}</h2>
           <div className="mb-6 text-gray-700 bg-gray-100 rounded-lg">
             <img
               className="rounded-lg w-full h-48 object-cover mb-2"
-              src={products.image}
-              alt={products.name}
+              src={product.imageUrl}
+              alt={product.name}
             />
-            <div className="p-4">{products.description}</div>
+            <div className="p-4">{product.description}</div>
           </div>
           <div className="mb-6 bg-gray-100 p-4 rounded-md">
             <h3 className="text-lg mb-4 font-semibold">Nutritional Facts</h3>
             <ul className="list-disc list-inside my-2">
-              {products.nutritional_facts &&
-                products.nutritional_facts.map((key, index) => (
+              {product.nutritional_facts &&
+                product.nutritional_facts.map((key, index) => (
                   <li key={index} className="text-gray-600">
-                    {key.nutrients}: {key.quantity}
+                    {key.nutrient}: {key.quantity}
                   </li>
                 ))}
             </ul>
           </div>
           <div className="bg-gray-100 p-4 rounded-md">
             <h3 className="text-lg mb-4 font-semibold">Benefits</h3>
-            <p className="text-gray-700 text-md">{products.benefits}</p>
+            <p className="text-gray-700 text-md">{product.benefits}</p>
           </div>
         </div>
       )}
