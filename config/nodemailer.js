fd1684ed6cc73340c31aa633f3869d4be7c53175
Aswin K <aswinkarat@gmail.com>
2023-10-17 08:58:08 +0530

    Feat: Changed mail service from gmail to outlook

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -3,25 +3,13 @@ import nodemailer from "nodemailer";
 const email = process.env.EMAIL;
 
 // for mailing through Outlook
-
-/* export const transporter = nodemailer.createTransport({
+export const transporter = nodemailer.createTransport({
   host: "smtp-mail.outlook.com", // hostname
   secureConnection: false, // TLS requires secureConnection to be false
   port: 587, // port for secure SMTP
   tls: {
     ciphers: "SSLv3",
   },
-  auth: {
-    user: "mymail@outlook.com",
-    pass: "myPassword",
-  },
-}); */
-
-// for mailing through gmail
-export const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
   auth: {
     user: email,
     pass: process.env.EMAIL_PASS,
