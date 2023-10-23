20d5f95434b63d730dc9da4b54fdcfd43e1fff46
emvk09 <edwinmoncy@gmail.com>
2023-10-24 01:40:19 +0530

    Fix: npm update

--- app/(guest_pages)/products/page.jsx
+++ app/(guest_pages)/products/page.jsx
@@ -2,18 +2,18 @@ import ClientProductPage from "@components/ClientProductPage";
 import { inDevEnvironment } from "@config/ProdDev";
 
 async function fetchProducts() {
-  let response = "";
+  let response = null;
   if (inDevEnvironment) {
     response = await fetch(`http://localhost:3000/api/products`);
   } else {
     response = await fetch(`${process.env.PROD_DOMAIN}/api/products`);
   }
-
   if (!response.status === 200) {
     // This will activate the closest `error.js` Error Boundary
     throw new Error("Failed to fetch data");
   }
-  return response.json();
+  const data = await response.json();
+  return data;
 }
 
 export default async function ProductsPage() {
