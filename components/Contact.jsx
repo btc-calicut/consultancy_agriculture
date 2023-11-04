0c314e308c05085bb8b718c2a790788bce868480
emvk09 <edwinmoncy@gmail.com>
2023-11-05 01:44:03 +0530

    Fix: Styling changes

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -6,10 +6,10 @@ import phone from "@public/images/phone.png";
 export default function Contact() {
   return (
     <div id="contact" className="relative overflow-hidden bg-zinc-50 w-full">
-      <div className="z-10 absolute -bottom-32 -left-40 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-      <div className="z-10 absolute -bottom-40 -left-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-      <div className="z-10 absolute -top-40 -right-0 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
-      <div className="z-10 absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
+      <div className="z-10 absolute -bottom-32 -left-40 w-80 h-80 border-4 border-gray-300 rounded-full border-opacity-30 border-t-8" />
+      <div className="z-10 absolute -bottom-40 -left-20 w-80 h-80 border-4 border-gray-300 rounded-full border-opacity-30 border-t-8" />
+      <div className="z-10 absolute -top-40 -right-0 w-80 h-80 border-4 border-gray-300 rounded-full border-opacity-30 border-t-8" />
+      <div className="z-10 absolute -top-20 -right-20 w-80 h-80 border-4 border-gray-300 rounded-full border-opacity-30 border-t-8" />
       <div className="py-20 px-6 sm:px-10 md:px-14 lg:px-36 sm:gap-x-20 flex flex-col-reverse gap-y-2 sm:flex-row sm:items-center sm:justify-center">
         <div className="z-20 relative sm:w-80 p-2 shadow-md rounded-lg hover:scale-105 transition ease-in-out duration-300">
           <Carousel dots={false} autoplay>
